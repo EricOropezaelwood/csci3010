90e58f3aa275accfddd4b950856350eb311631b8
Eric Oropezaelwood <eric.oropezaelwood@gmail.com>
2018-08-30 15:47:38 -0600

    Create main.cpp

---
+++ PE1/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Point.h"
+
+using namespace std;
+
+int main()
+{
+	// create first point
+	Point one(1.1, 2.2, 3.3);
+	// create second point
+	Point two(11.11, 12.12, 13.13);
+
+	// store the coordinates of both points
+	double oneX = one.getX();
+	double oneY = one.getY();
+	double oneZ = one.getZ();
+	double twoX = two.getX();
+	double twoY = two.getY();
+	double twoZ = two.getZ();
+
+	// calculate the distance between the two points
+	double distance = one.euclDist3D(oneX, oneY, oneZ, twoX, twoY, twoZ);
+	cout <<"The euclidean distance between the two points is: " << distance << endl;
+
+	// display the coordinates
+	cout <<"X coordinate of point one: " << oneX << endl;
+	cout <<"Y coordinate of point one: " << oneY << endl;
+	cout <<"Z coordinate of point one: " << oneZ << endl;
+
+	cout <<"X coordinate of point two: " << twoX << endl;
+	cout <<"Y coordinate of point two: " << twoY << endl;
+	cout <<"Z coordinate of point two: " << twoZ << endl;
+
+	return 0;
+}
